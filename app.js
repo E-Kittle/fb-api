e476122c034d5b1b8b2c06250ba0f193301f75fb
Elisabeth Kittle <elisabethkittle@gmail.com>
2021-11-02 13:31:22 -0400

    Attempt to fix cors error

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.set('view engine', 'pug');
 
 //Middleware
 app.use(cors({
-  origin: 'http://odinbook.elisabethoconnor.com/'
+  origin: 'http://odinbook.elisabethoconnor.com'
 }))
 
 app.use(logger('dev'));
